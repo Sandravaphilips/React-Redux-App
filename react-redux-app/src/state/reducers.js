15d2a4c1493332a7cf2a7c99b27d829afb53ea0c
Sandrava Philips <lacasandra1@gmail.com>
2019-10-10 14:01:51 +0100

    built my reducer function

--- react-redux-app/src/state/reducers.js
+++ react-redux-app/src/state/reducers.js
@@ -1,1 +1,13 @@
 import * as types from "./actionTypes";
+
+const initialPlaylist = [];
+
+export default function reducer(state = initialPlaylist, action) {
+    switch(action.type) {
+        case types.FETCH_CONTENT:
+            return state.concat(action.payload)
+
+        default: 
+            return state;
+    }
+}
