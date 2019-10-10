6af0b2dfd4eb35bf9048964b4ab164b9d666ebaf
Sandrava Philips <lacasandra1@gmail.com>
2019-10-10 13:09:45 +0100

    built my navigation component

---
+++ react-redux-app/src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navigation() {
+    return (
+        <nav>
+            <a>Home</a>
+            <a>Services</a>
+            <a>Contact</a>
+            <a>Create your Playlist</a>
+        </nav>
+        
+    )
+}
